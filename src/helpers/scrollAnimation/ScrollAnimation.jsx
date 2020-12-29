be731798cebd76cc37d12d2cd61161ae51c514a6
jonathan <jonathangomezit@gmail.com>
2020-12-29 04:54:44 -0300

    Modulo de Scroll optimizado con el hook que corresponde useEffect

--- src/helpers/scrollAnimation/ScrollAnimation.jsx
+++ src/helpers/scrollAnimation/ScrollAnimation.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
 const ScrollAnimation = () => {
+    const [currentScroll, setCurrentScroll] = React.useState(document.documentElement.scrollTop)
     
-        const handleScroll = () => {
+    React.useEffect(()=> {
 
+        const handleScroll = () => {
+            
             const divs = document.querySelectorAll('.effectScroll');
-            const top = document.documentElement.scrollTop;
+            setCurrentScroll(document.documentElement.scrollTop)
             const finalHeight = window.innerHeight * 0.90;
 
             divs.forEach(div => {
                 
-                if(div.offsetTop - finalHeight < top){
+                if(div.offsetTop - finalHeight < currentScroll){
                     div.style.opacity= 1;
                     div.classList.add('show-effect');
                 }
             });
         }
 
-    window.addEventListener('scroll', handleScroll)
+        window.addEventListener('scroll', handleScroll)
+        return function clear(){ 
+            window.removeEventListener('scroll', handleScroll);
+        }
 
+    });
+        
     return(
         <></>
     )
